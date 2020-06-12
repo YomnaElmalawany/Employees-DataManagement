e792eded37b9c9019591aca380228f353b35952e
YomnaElmalawany <32571876+YomnaElmalawany@users.noreply.github.com>
2020-06-12 11:53:32 +0200

    Added Edit  & delete view + client-side validation (create, Edit)

--- EmployeesData/EmployeesData/Controllers/EmployeesController.cs
+++ EmployeesData/EmployeesData/Controllers/EmployeesController.cs
@@ -42,5 +42,72 @@ namespace EmployeesData.Controllers
             return View();
         }
 
+        // GET: Employees/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = _employee.GetEmployeeById((int)id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public IActionResult Edit(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _employee.EditEmployee(employee);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    //if (_employee.GetEmployeeById(employee.Id) == null)
+                    //{
+                    return NotFound();
+                    //}
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = _employee.GetEmployeeById((int)id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _employee.DeleteEmployee(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
